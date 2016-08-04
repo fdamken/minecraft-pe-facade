d3ca53b79bf4ca1bb7b80ee4455ffd7a4a7eebd7
Fabian Damken <fabian.damken@gmail.com>
2016-08-04 19:50:34 +0200

    Added (not implemented!) protocol data types.

---
+++ src/main/java/de/fdamken/minecraft/pe/facade/protocol/datatype/DataType.java
@@ -0,0 +1,53 @@
+/*
+ * #%L
+ * Minecraft Pocket Edition Facade
+ * %%
+ * Copyright (C) 2016 - 2016 Fabian Damken
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package de.fdamken.minecraft.pe.facade.protocol.datatype;
+
+import io.netty.buffer.ByteBuf;
+
+/**
+ * A data type is a fix defined type of data that can be read from a
+ * {@link ByteBuf buffer} and can be written to a {@link ByteBuf buffer}.
+ * 
+ * @param <T>
+ *            The Java type that represents this type.
+ */
+public interface DataType<T> {
+    /**
+     *
+     * @return The stored value as a regular Java type.
+     */
+    T get();
+
+    /**
+     * Reads the given {@link ByteBuf buffer} and sets the value of this type.
+     *
+     * @param buffer
+     *            The {@link ByteBuf buffer} to read.
+     */
+    void read(ByteBuf buffer);
+
+    /**
+     * Writes the value of this date into the given {@link ByteBuf buffer}.
+     *
+     * @param buffer
+     *            The {@link ByteBuf buffer} to write into.
+     */
+    void write(ByteBuf buffer);
+}
---
+++ src/main/java/de/fdamken/minecraft/pe/facade/protocol/datatype/LongType.java
@@ -0,0 +1,57 @@
+/*
+ * #%L
+ * Minecraft Pocket Edition Facade
+ * %%
+ * Copyright (C) 2016 - 2016 Fabian Damken
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package de.fdamken.minecraft.pe.facade.protocol.datatype;
+
+import io.netty.buffer.ByteBuf;
+
+public class LongType implements DataType<Long> {
+    /**
+     * {@inheritDoc}
+     *
+     * @see de.fdamken.minecraft.pe.facade.protocol.datatype.DataType#get()
+     */
+    @Override
+    public Long get() {
+        // TODO Auto-generated method body.
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see de.fdamken.minecraft.pe.facade.protocol.datatype.DataType#read(io.netty.buffer.ByteBuf)
+     */
+    @Override
+    public void read(final ByteBuf buffer) {
+        // TODO Auto-generated method body.
+
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see de.fdamken.minecraft.pe.facade.protocol.datatype.DataType#write(io.netty.buffer.ByteBuf)
+     */
+    @Override
+    public void write(final ByteBuf buffer) {
+        // TODO Auto-generated method body.
+
+    }
+}
